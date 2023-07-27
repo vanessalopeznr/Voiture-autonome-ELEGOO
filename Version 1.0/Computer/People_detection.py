5a5e3c1ace9c70f6ef8436fd97e4dd124ff158c0
Vanessa Lopez <123451768+vanessalopeznr@users.noreply.github.com>
2023-07-27 13:00:15 +0200

    Update People_detection.py

--- Version 1.0/Computer/People_detection.py
+++ Version 1.0/Computer/People_detection.py
@@ -5,12 +5,14 @@ import cv2
 from ultralytics import YOLO
 import socket
 
-def receive_command(command):
+address="192.168.0.100"
+
+def send_command(command):
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Raspberry Pi's IP address and the port for command communication
-    server_address = ('192.168.0.100', 8001)
+    server_address = (address, 8001)
 
     # Connect to the server
     client_socket.connect(server_address)
@@ -26,10 +28,10 @@ def receive_command(command):
 model = YOLO('yolov8n.pt')
 
 # Open the video file
-source = "http://192.168.0.100:8000/stream.mjpg"
-#source = '0'
+source = "http://" + address + ":8000/stream.mjpg"
+#source = '0' #For the camera
 
-results = model.predict(source, stream=True, conf=0.5)  # generator of Results objects
+results = model.predict(source, stream=True, classes=0, max_det=1, conf=0.5)  # generator of Results objects
 
 for r in results:
     print('------------------------------------------------------')
@@ -39,9 +41,23 @@ for r in results:
         pers = int(box.cls[0])
         if pers==0:
             # box.xyxy[0] Coordinates of bounding boxes in the form (x1, y1, x2, y2) where (x1, y1) is the top-left corner of the bounding box, and (x2, y2) denotes the bottom-right corner
-            centerx=((box.xyxy[0][2]-box.xyxy[0][0])/2)+box.xyxy[0][0]
-            centery=((box.xyxy[0][3]-box.xyxy[0][1])/2)+box.xyxy[0][1]
+            width=box.xyxy[0][2]-box.xyxy[0][0]
+            height=box.xyxy[0][3]-box.xyxy[0][1]
+            area=width*height
+
+            centerx=(width/2)+box.xyxy[0][0]
+            centery=(height/2)+box.xyxy[0][1]
             cv2.circle(image, (int(centerx), int(centery)), 5, (0, 0, 255),-1)
+
+            if area < 30000:
+                command_height="fast"
+
+            elif 30000 < area < 90000:
+                command_height="normal"
+
+            else:
+                command_height="slow"
+            
 
             if centerx>380:
                 command_width="right"
@@ -52,16 +68,7 @@ for r in results:
             else: 
                 command_width='None'
 
-            if centery>260:
-                command_height="front"
-
-            elif centery<220:
-                command_height="back"
-             
-            else:
-                command_height='None'
-
-            receive_command(command_width + '/' + command_height)
+            send_command(command_width + '/' + command_height)
             print(command_width,command_height)
 
     cv2.imshow("result", image)
